c4316fd701c75c68e5e069c135abc3f6e98f4bd5
naiik21 <nratia22@jaumebalmes.net>
2023-10-03 19:28:46 +0200

    practicas 2 arrays i matrius

---
+++ Practica2.php
@@ -0,0 +1,106 @@
+<!DOCTYPE html>
+<html lang="es">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
+    <?php
+        $taules= array(); // Array buida, on es posaran el numero de comensals
+        // Afegim a taules un numero random del 0 al 5 (el numero de comensals)
+        for($i=0; $i<=9; $i++){
+            array_push($taules, rand(0, 5));
+        }
+
+        // Iterem per imprimer la taula amb el nº de comensals
+        for($i=0; $i<count($taules); $i++){
+    ?>
+            <ul>
+    <?php
+                // Si té 0 comensals
+                if($taules[$i]==0){
+    ?>
+                    <li>
+                        <?php echo"La taula ".$i." esta buida"?>
+                    </li>
+    <?php
+                // Si té 5 comensals
+                }else if($taules[$i]==5){
+    ?>
+                    <li>
+                        <?php echo"La taula ".$i." esta plena"?>
+                    </li>
+    <?php    
+                // Si té de 1-4 comensals
+                }else{
+    ?>
+                    <li>
+                        <?php echo"La taula ".$i." té ".$taules[$i]." comensals"?>
+                    </li>
+    <?php            
+                }
+    ?>  
+            </ul>
+    <?php            
+                
+        }
+    ?>      
+
+    <br>
+    <br>
+    <br>
+
+    <?php
+        $hotel= array(); // Array buida, on es posaran el numero de plantes, habitacions i persones
+        // Afegim a hotel una serie d'arrays que representen les plantes
+        for($i=0; $i<5; $i++){
+            array_push($hotel, array());
+            // I dintre de les plantes afegirem noves arrays que seran les habitacion i es generara un numero random de persones
+            for($a=0; $a<10; $a++){
+                array_push($hotel[$i], rand(0, 4));
+            }
+        }
+
+        // Iterem per imprimer la planta amb el nº de habitacio
+        for($i=0; $i<count($hotel); $i++){ 
+            // I en l'habitació quantes persones
+            for($a=0; $a<count($hotel[$i]); $a++){
+    ?>
+            <ul>
+    <?php
+                // Si té 0 persones
+                if($hotel[$i][$a]==0){
+    ?>
+                    <li>
+                        <?php echo"A l'habitació ".$a." de la planta " .$i. " esta buida"?>
+                    </li>
+    <?php
+                // Si té 4 persones
+                }else if($hotel[$i][$a]==4){
+    ?>
+                    <li>
+                        <?php echo"A l'habitació ".$a." de la planta " .$i. " esta plena"?>
+                    </li>
+    <?php    
+                // Si té de 1-3 persones
+                }else{
+    ?>
+                    <li>
+                        <?php echo"A l'habitació ".$a." de la planta " .$i." té ".$hotel[$i][$a]." persones"?>
+                    </li>
+                    
+    <?php            
+                }
+    ?>      
+            </ul>
+    <?php            
+                
+        }
+    ?>
+        <br>
+    <?php
+    }
+    ?>  
+</body>
+</html>
--- README.md
+++ README.md
@@ -1,2 +1,7 @@
 # Persona en PHP
-![image](markdown.png)
+1.El restaurant estarà format per 10 taules, on cada taula hi caben fins a 5 comensals. Volem una sortida a la terminal tal que: Recomanació: Feu, individualment o per parelles, el plantejament, sense codi, de la possible solució de l’exercici. 
+![](p2_T1.png)
+
+2.Es tracta de programar la gestió d’habitacions d’un hotel. Imagineu un hotel amb 5 plantes i 10 habitacions en cadascuna de les plantes. Es vol guardar el nombre de clients que hi ha en cada habitació. Com a màxim hi pot haver 4 clients per habitació.Recomanació: Feu, individualment o per parelles, el plantejament, sense codi, de la possible solució de l’exercici. 
+![](p2_T2.png)
+
